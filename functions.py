3fe1c2e62b6da6de2f188c636788d841cc11d607
Brian Ross <brian.ross@students.makeschool.com>
2020-09-10 17:45:32 -0400

    Functions tutorial

---
+++ functions.py
@@ -0,0 +1,34 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+result2 = area(3, 9)
+result3 = area(1, 7)
+
+
+def subtract(num1, num2):
+    subanswer = num1 - num2
+    return subanswer
+
+subresult1 = subtract(10, 4)
+subresult2 = subtract(11, 5)
+
+print(subresult1, subresult2)
+
+
+def divide(num1, num2):
+    divanswer = num1 / num2
+    return divanswer
+
+def add(num1, num2):
+    addanswer = num1 + num2
+    return addanswer
+
+addanswer1 = add(3,4)
+addanswer2 = add(3,7)
+
+print(addanswer1, addanswer2)
+
+
